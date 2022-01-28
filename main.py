b95a5ca7286e965068682586fee4809acb0f1aac
hasanaliozkan-dev <hasanaliozkan-dev@outlook.com>
2022-01-28 22:15:41 +0300

    "comments are added"

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras import datasets,layers,models
+import matplotlib.pyplot as plt             #to view images on a grid plot.
+from tensorflow.keras import datasets,layers,models     #to create CNN
 
-(training_images,training_labels),(testing_images,testing_labels)=datasets.cifar10.load_data();
-training_images,testing_images = training_images/255,testing_images/255
+(training_images,training_labels),(testing_images,testing_labels)=datasets.cifar10.load_data() #dataset comes from the tensorflow
+training_images,testing_images = training_images/255,testing_images/255     # we divide because every pixel should get value between 0 and 255
 
-class_name = ["Plane","Car","Bird","Cat","Deer","Dog","Frog","Horse","Sheep","Truck"]
+class_name = ["Plane","Car","Bird","Cat","Deer","Dog","Frog","Horse","Sheep","Truck"]  # The first ten classes
 
+
+#This loops for arrange the first 16 images in a plot
 for i in range(16):
     plt.subplot(4,4,i+1)
     plt.xticks([])
@@ -17,24 +17,36 @@ for i in range(16):
 
 plt.show()
 
+#subsampling the example space in order to speed up the proccess
 training_images = training_images[:20000]
 training_labels = training_labels[:20000]
 testing_images = testing_images[:4000]
 testing_labels = testing_labels[:4000]
 
-model = models.Sequential()
+model = models.Sequential() # our model will be sequential
+#input layer
 model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)))
+#Hiddeb Layers
 model.add(layers.MaxPooling2D(2,2))
 model.add(layers.Conv2D(64,(3,3),activation='relu'))
 model.add(layers.MaxPooling2D(2,2))
 model.add(layers.Conv2D(64,(3,3),activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64,activation='relu'))
+
+# output layer
 model.add(layers.Dense(10,activation='softmax'))
+
+# compiling the model
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+#fitting the model
 model.fit(training_images,training_labels,epochs=10,validation_data=(testing_images,testing_labels))
 
+#Evaluating the model
 loss,accuracy = model.evaluate(testing_images,testing_labels)
 print(f"Loss: {loss}")
 print(f"Accuracy: {accuracy}")
+
+#save model to test
 model.save('image_classifier.model')
--- testing.py
+++ testing.py
@@ -5,14 +5,22 @@ from tensorflow.keras import datasets,layers,models
 
 class_name = ["Plane","Car","Bird","Cat","Deer","Dog","Frog","Horse","Sheep","Truck"]
 
+#loading the model
 model = models.load_model('image_classifier.model')
 
+#loading the test image
 img = cv.imread("plane.jpg")
+#images have BGR format but we worked(matplotlib) on RGB format so we convert it.
 img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+
+#in order to show image
 plt.imshow(img,cmap=plt.cm.binary)
 
+#lets predict the classes
 prediction = model.predict(np.array([img])/255)
+
 index = np.argmax(prediction)                   #argmax gives index of the maximum value
 
+#and result is here
 print(f"Prediction : {class_name[index]}")
 plt.show()
